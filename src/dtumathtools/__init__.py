52ee614bc0539c9acad1aedf6400d92838df493d
Christian Mikkelstrup <cmi@trackman.com>
2022-12-29 13:29:39 +0100

    divergence and rotation of vector fields

--- src/dtumathtools/__init__.py
+++ src/dtumathtools/__init__.py
@@ -1,18 +1,4 @@
 from . import dtuplot
-from .helpers import taylor,gradient,hessian
+from .helpers import taylor, gradient, hessian, div, rot
 
-
-# # from sympy.external import import_module
-
-# from . import quiverplot as dtuplot
-
-# # dtuplot = import_module("spb")
-# import spb as dtuplot
-
-__all__ = ["dtuplot","taylor","gradient","hessian"]
-
-
-
-
-
-
+__all__ = ["dtuplot", "taylor", "gradient", "hessian", "div", "rot"]
--- src/dtumathtools/helpers.py
+++ src/dtumathtools/helpers.py
@@ -2,32 +2,69 @@ from sympy import *
 from functools import reduce
 
 
-def __extract_vars(expr):
-    return sorted(list(map(lambda x : str(x),expr.free_symbols)))
+def _extract_vars(expr):
+    return sorted(list(map(str, expr.free_symbols)))
 
-def taylor(expr,vars,degree):
+
+def taylor(expr, vars, degree):
     if len(vars) != 2 and len(vars) != 4:
         raise Exception("taylor is only defined for one or two variables")
-    if len(vars) == 2: return series(expr,vars[0],vars[1],n=degree).removeO()
+    if len(vars) == 2:
+        return series(expr, vars[0], vars[1], n=degree).removeO()
     P_n = S(0)
-    x,x0,y,y0 = vars
+    x, x0, y, y0 = vars
     for i in range(degree):
         for j in range(degree - i):
-            P_n += expr.diff(x,i).diff(y,j).subs([(x,x0),(y,y0)]) * (x - x0) ** i * (y - y0) ** j / (factorial(i)  * factorial(j))
+            P_n += (
+                expr.diff(x, i).diff(y, j).subs([(x, x0), (y, y0)])
+                * (x - x0) ** i
+                * (y - y0) ** j
+                / (factorial(i) * factorial(j))
+            )
 
     return P_n
 
 
-def gradient(expr):
-    var = __extract_vars(expr)
-    if(len(var) == 1):
+def gradient(expr, var=None):
+    var = _extract_vars(expr) if var is None else var
+    if len(var) == 1:
         return expr.diff(var[0])
     return Matrix([expr.diff(v) for v in var])
 
-def hessian(expr,var= None):
-    var = __extract_vars(expr) if var is None else var
-    return Matrix([[reduce(lambda e,s : e.diff(s),[x,y],expr) for x in var] for y in var])
+
+def hessian(expr, var=None):
+    var = _extract_vars(expr) if var is None else var
+    return Matrix(
+        [[reduce(lambda e, s: e.diff(s), [x, y], expr) for x in var] for y in var]
+    )
 
 
+def _extract_field_vars(V, var, namefunc):
+    var = _extract_vars(V) if var is None else var
+    x, y, z = symbols("x,y,z")
+    if len(var) < 3 and all([f in [x, y, z] for f in var]):
+        var = [x, y, z]
+    elif len(var) != 3:
+        raise Exception(
+            f"{namefunc} found the variables {var}, but expected 3 variables. \
+            Try specifying the variables using the 'var' keyword argument like: {namefunc}(V, var=(x,y,z))."
+        )
+    return var
 
 
+def div(V, var=None):
+    var = _extract_field_vars(V, var, "div")
+
+    return sum([V[i].diff(f) for i, f in enumerate(var)])
+
+
+def rot(V, var=None):
+    var = _extract_field_vars(V, var, "rot")
+
+    return Matrix(
+        [
+            V[2].diff(var[1]) - V[1].diff(var[2]),
+            V[0].diff(var[2]) - V[2].diff(var[0]),
+            V[1].diff(var[0]) - V[0].diff(var[1]),
+        ]
+    )
