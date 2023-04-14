f616f325c98cba8440fe1140f2bdc032bb013580
karljohan <karljohan@maastrup.dk>
2023-04-14 10:59:20 +0200

    Added docstring to plot_boundary

--- src/dtumathtools/dtuplot/boundaryplot.py
+++ src/dtumathtools/dtuplot/boundaryplot.py
@@ -2,6 +2,30 @@ from spb.functions import plot_parametric
 from sympy import Matrix
 
 def plot_boundary(param_func: Matrix, *args, **kwargs):
+    """ A function for plotting the boundary of a 2D
+        area
+        
+        ## Input:
+            param_func: A matrix with functions [x(t), y(t)] determining 
+                        x and y-coordinates of area to be plotted
+            (u, u_min, u_max): A tuple with variable and its limits either as list or two seperate values
+            (v, v_min, v_max): Same as above but for other variable
+
+            rendering_kw: A dictionary forwarded to dtuplot.plot(), see SPB docs for reference
+
+            
+        ### Keyword arguments:
+            color [Optional]: A string to set color boundary with. With no argument color = 'blue'
+            show [Optional]: Boolean, if 'True': show plot, other just return object without plotting
+                            defaults to 'True'
+
+
+
+        ## Returns:
+            plot: A SPB-plot object
+
+            
+    """
     if len(param_func) == 2:
         bounds = []
         for i, arg in enumerate(args):
