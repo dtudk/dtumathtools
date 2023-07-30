3105a62b670f912e48d0d53fb9e7b7032c3b9a5f
Christian Mikkelstrup <cmi@trackman.com>
2023-07-30 12:27:35 +0200

    Testing plotting with different backends

--- tests/test_dtuplot.py
+++ tests/test_dtuplot.py
@@ -1,6 +1,6 @@
 from dtumathtools import *
 from sympy import *
-from spb import MB, PB, BB
+from spb import MB, PB, BB, KB, MAB
 import pytest
 import numpy as np
 import re
@@ -12,21 +12,21 @@ del test
 
 def test_quiver():
     # matplotlib
-    dtuplot.quiver(Matrix([1, 2, 3]), Matrix([4, 5, 6]), {"color": "red"}, show=False)
-    dtuplot.quiver([1, 2, 3], [4, 5, 6], show=False)
-    dtuplot.quiver([1, 2], [4, 5], show=False)
-    dtuplot.quiver(Matrix([1, 2]), Matrix([4, 5]), show=False)
-    dtuplot.quiver(1, 2, 0, 0, 0, 3, rendering_kw={"color": "orange"}, show=False)
-    dtuplot.quiver(1, 2, 1, 2, show=False)
-    dtuplot.quiver((1, 2), (1, 2), show=False)
-    dtuplot.quiver(np.array([1, 2]), (1, 2), show=False)
-    dtuplot.quiver(np.array([1, 2]), np.array([1, 2]), show=False)
-    dtuplot.quiver(np.array([1, 2]), Matrix([1, 2]), show=False)
-    dtuplot.quiver(np.array([1, 2, 3]), (1, 2, 3), show=False)
-    dtuplot.quiver(np.array([1, 2, 3]), np.array([1, 2, 3]), show=False)
-    dtuplot.quiver(np.array([1, 2, 3]), Matrix([1, 2, 3]), show=False)
-    dtuplot.quiver([1, 2, 3], Matrix([1, 2, 3]), label="123", show=False)
-    dtuplot.quiver([1, 2, 3], Matrix([1, 2, 3]), label=["123"], show=False)
+    dtuplot.quiver(Matrix([1, 2, 3]), Matrix([4, 5, 6]), {"color": "red"}, backend=MB, show=False)
+    dtuplot.quiver([1, 2, 3], [4, 5, 6], backend=MB, show=False)
+    dtuplot.quiver([1, 2], [4, 5], backend=MB, show=False)
+    dtuplot.quiver(Matrix([1, 2]), Matrix([4, 5]), backend=MB, show=False)
+    dtuplot.quiver(1, 2, 0, 0, 0, 3, rendering_kw={"color": "orange"}, backend=MB, show=False)
+    dtuplot.quiver(1, 2, 1, 2, backend=MB, show=False)
+    dtuplot.quiver((1, 2), (1, 2), backend=MB, show=False)
+    dtuplot.quiver(np.array([1, 2]), (1, 2), backend=MB, show=False)
+    dtuplot.quiver(np.array([1, 2]), np.array([1, 2]), backend=MB, show=False)
+    dtuplot.quiver(np.array([1, 2]), Matrix([1, 2]), backend=MB, show=False)
+    dtuplot.quiver(np.array([1, 2, 3]), (1, 2, 3), backend=MB, show=False)
+    dtuplot.quiver(np.array([1, 2, 3]), np.array([1, 2, 3]), backend=MB, show=False)
+    dtuplot.quiver(np.array([1, 2, 3]), Matrix([1, 2, 3]), backend=MB, show=False)
+    dtuplot.quiver([1, 2, 3], Matrix([1, 2, 3]), label="123", backend=MB, show=False)
+    dtuplot.quiver([1, 2, 3], Matrix([1, 2, 3]), label=["123"], backend=MB, show=False)
     # plotly
     dtuplot.quiver(
         Matrix([1, 2, 3]), Matrix([4, 5, 6]), show=False, backend=PB
@@ -55,6 +55,59 @@ def test_quiver():
     dtuplot.quiver(np.array([1, 2]), (1, 2), backend=BB, show=False)
     dtuplot.quiver(np.array([1, 2]), np.array([1, 2]), backend=BB, show=False)
     dtuplot.quiver(np.array([1, 2]), Matrix([1, 2]), backend=BB, show=False)
+    # k3d
+    with pytest.warns(
+        UserWarning,
+        match="K3DBackend only works properly within Jupyter Notebook"
+    ):
+        dtuplot.quiver(Matrix([1, 2, 3]), Matrix([4, 5, 6]), backend=KB, show=False)
+        dtuplot.quiver([1, 2, 3], [4, 5, 6], backend=KB, show=False)
+        dtuplot.quiver(1, 2, 0, 0, 0, 3, backend=KB, show=False)
+        dtuplot.quiver(np.array([1, 2, 3]), (1, 2, 3), backend=KB, show=False)
+        dtuplot.quiver(np.array([1, 2, 3]), np.array([1, 2, 3]), backend=KB, show=False)
+        dtuplot.quiver(np.array([1, 2, 3]), Matrix([1, 2, 3]), backend=KB, show=False)
+        dtuplot.quiver([1, 2, 3], Matrix([1, 2, 3]), label="123", backend=KB, show=False)
+        dtuplot.quiver([1, 2, 3], Matrix([1, 2, 3]), label=["123"], backend=KB, show=False)
+    # mayavi
+    # A lot of things in the Mayavi toolbox is deprecated...
+    with pytest.warns(
+        DeprecationWarning
+    ):
+        # Options to hide screen popping up
+        from mayavi import mlab
+        mlab.options.offscreen = True
+        dtuplot.quiver(Matrix([1, 2, 3]), Matrix([4, 5, 6]), backend=MAB, show=False, warning=False)
+        dtuplot.quiver([1, 2, 3], [4, 5, 6], backend=MAB, show=False, warning=False)
+        dtuplot.quiver(1, 2, 0, 0, 0, 3, backend=MAB, show=False, warning=False)
+        dtuplot.quiver(np.array([1, 2, 3]), (1, 2, 3), backend=MAB, show=False, warning=False)
+        dtuplot.quiver(np.array([1, 2, 3]), np.array([1, 2, 3]), backend=MAB, show=False, warning=False)
+        dtuplot.quiver(np.array([1, 2, 3]), Matrix([1, 2, 3]), backend=MAB, show=False, warning=False)
+        dtuplot.quiver([1, 2, 3], Matrix([1, 2, 3]), label="123", backend=MAB, show=False, warning=False)
+        dtuplot.quiver([1, 2, 3], Matrix([1, 2, 3]), label=["123"], backend=MAB, show=False, warning=False)
+    
+    with pytest.raises(
+        NotImplementedError,
+        match="Mayavi backend does not support 2D vector plots!"
+    ):
+        dtuplot.quiver([1, 2], [4, 5], backend=MAB, show=False, warning=False)
+    
+    with pytest.warns(
+        UserWarning,
+        match="Because of the Mayavi backend, the origin of the vector might be slightly off. To supress this warning, set 'warning=False'"
+    ):
+        dtuplot.quiver(Matrix([100,100,100]), Matrix([10,20,30]), backend=dtuplot.MAB)
+    
+    with pytest.raises(
+        NotImplementedError,
+        match="K3D backend does not support 2D vector plots!"
+    ):
+        dtuplot.quiver(Matrix([1, 2]), Matrix([0,10]), backend=dtuplot.KB)
+    
+    with pytest.raises(
+        NotImplementedError,
+        match="Bokeh backend does not support 3D vector plots!"
+    ):
+        dtuplot.quiver(Matrix([1, 2, 3]), Matrix([0,10,100]), backend=dtuplot.BB)
     
     with pytest.raises(
         ValueError,
@@ -62,25 +115,25 @@ def test_quiver():
             "Error! Wrong format used in quiver. Got [1. 2. 3. 4.] as starting point(s) and [4. 5. 6. 7.] as ending point(s)!"
         ),
     ):
-        dtuplot.quiver(Matrix([1, 2, 3, 4]), Matrix([4, 5, 6, 7]), show=False)
+        dtuplot.quiver(Matrix([1, 2, 3, 4]), Matrix([4, 5, 6, 7]), backend=MB, show=False)
 
     with pytest.raises(
         Exception,
         match="Error! Wrong format used in quiver. Got 7 arguments that could be start or direction vector coordinates!",
     ):
-        dtuplot.quiver(1, 2, 3, 4, 5, 6, 7, show=False)
+        dtuplot.quiver(1, 2, 3, 4, 5, 6, 7, backend=MB, show=False)
 
     with pytest.raises(
         Exception,
         match="Error! Wrong format used in quiver. Got 5 arguments that could be start or direction vector coordinates!",
     ):
-        dtuplot.quiver(1, 2, 3, 4, 5, show=False)
+        dtuplot.quiver(1, 2, 3, 4, 5, backend=MB, show=False)
 
     with pytest.raises(
         Exception,
         match="Error! Wrong format used in quiver. Got 3 arguments that could be start or direction vector coordinates!",
     ):
-        dtuplot.quiver(1, 2, 3, show=False)
+        dtuplot.quiver(1, 2, 3, backend=MB, show=False)
 
     with pytest.raises(
         AssertionError,
@@ -88,7 +141,7 @@ def test_quiver():
             "Error! Start and direction vectors must be provided (only two vectors)!"
         ),
     ):
-        dtuplot.quiver([1, 2], [3, 4], [5, 6], show=False)
+        dtuplot.quiver([1, 2], [3, 4], [5, 6], backend=MB, show=False)
 
     with pytest.raises(
         AssertionError,
@@ -96,7 +149,7 @@ def test_quiver():
             "Error! Start and direction vectors must be provided (only two vectors)!"
         ),
     ):
-        dtuplot.quiver([1, 2], show=False)
+        dtuplot.quiver([1, 2], backend=MB, show=False)
 
     with pytest.raises(
         ValueError,
@@ -104,23 +157,23 @@ def test_quiver():
             "Error! Wrong format used in quiver. Got [1.] as starting point(s) and [2.] as ending point(s)!"
         ),
     ):
-        dtuplot.quiver([1], [2], show=False)
+        dtuplot.quiver([1], [2], backend=MB, show=False)
 
     with pytest.raises(
         AssertionError,
         match="Error! Number of labels must be equal to number of arrows, or empty list!",
     ):
-        dtuplot.quiver([1, 2, 3], Matrix([1, 2, 3]), label=["123", "456"], show=False)
+        dtuplot.quiver([1, 2, 3], Matrix([1, 2, 3]), label=["123", "456"], backend=MB, show=False)
 
     with pytest.raises(
         AssertionError, match="Error! Label must be a list or a string!"
     ):
-        dtuplot.quiver([1, 2, 3], Matrix([1, 2, 3]), label=123, show=False)
+        dtuplot.quiver([1, 2, 3], Matrix([1, 2, 3]), label=123, backend=MB, show=False)
 
     with pytest.raises(
         NotImplementedError, match="Interactive quiver plots are not yet implemented!"
     ):
-        dtuplot.quiver([1, 2, 3], Matrix([1, 2, 3]), params=123, show=False)
+        dtuplot.quiver([1, 2, 3], Matrix([1, 2, 3]), params=123, backend=MB, show=False)
 
 
 def test_boundary():
@@ -137,6 +190,7 @@ def test_boundary():
         xlabel="some",
         ylabel="thing",
         zlabel="here",
+        backend=MB,
         show=False,
     )
 
@@ -171,6 +225,7 @@ def test_boundary():
             ylabel="thing",
             zlabel="here",
             show=False,
+            backend=MB,
         )
 
     with pytest.raises(
@@ -187,75 +242,153 @@ def test_boundary():
             ylabel="thing",
             zlabel="here",
             show=False,
+            backend=MB,
         )
 
 
 def test_scatterplot():
-    dtuplot.scatter(1, 2, show=False)
-    dtuplot.scatter(1, 2, 3, show=False)
-    dtuplot.scatter([1], [2], show=False)
-    dtuplot.scatter([[1]], [[2]], show=False)
-    dtuplot.scatter([1, 2, 3], [4, 5, 6], show=False)
-    dtuplot.scatter([1, 2, 3], [4, 5, 6], [7, 8, 9], show=False)
+    # matplotlib
+    dtuplot.scatter(1, 2, backend=MB, show=False)
+    dtuplot.scatter(1, 2, 3, backend=MB, show=False)
+    dtuplot.scatter([1], [2], backend=MB, show=False)
+    dtuplot.scatter([[1]], [[2]], backend=MB, show=False)
+    dtuplot.scatter([1, 2, 3], [4, 5, 6], backend=MB, show=False)
+    dtuplot.scatter([1, 2, 3], [4, 5, 6], [7, 8, 9], backend=MB, show=False)
     dtuplot.scatter(
-        np.array([1, 2, 3]), np.array([4, 5, 6]), np.array([7, 8, 9]), show=False
+        np.array([1, 2, 3]), np.array([4, 5, 6]), np.array([7, 8, 9]), backend=MB, show=False
     )
-    dtuplot.scatter(np.array([1]), np.array([4]), np.array([7]), show=False)
-    dtuplot.scatter(np.array([1, 2, 3]), np.array([4, 5, 6]), show=False)
+    dtuplot.scatter(np.array([1]), np.array([4]), np.array([7]), backend=MB, show=False)
+    dtuplot.scatter(np.array([1, 2, 3]), np.array([4, 5, 6]), backend=MB, show=False)
     with pytest.warns(match="np.array with more than 1 dimension"):
-        dtuplot.scatter(np.array([[1, 2, 3]]), np.array([[4, 5, 6]]), show=False)
+        dtuplot.scatter(np.array([[1, 2, 3]]), np.array([[4, 5, 6]]), backend=MB, show=False)
     with pytest.warns(match="np.array with more than 1 dimension"):
         dtuplot.scatter(
             np.array([[1, 2, 3], [7, 8, 9]]),
             np.array([[4, 5, 6], [4, 5, 6]]),
-            show=False,
+            backend=MB, show=False,
         )
-    dtuplot.scatter(np.array([1]), np.array([4]), show=False)
+    dtuplot.scatter(np.array([1]), np.array([4]), backend=MB, show=False)
     dtuplot.scatter(
-        Matrix([1, 2, 3]), Matrix([4, 5, 6]), show=False
+        Matrix([1, 2, 3]), Matrix([4, 5, 6]), backend=MB, show=False
     )  # different behavior to the np.array version
-    dtuplot.scatter(Matrix([1, 2]), Matrix([4, 5]), show=False)
+    dtuplot.scatter(Matrix([1, 2]), Matrix([4, 5]), backend=MB, show=False)
     with pytest.raises(
         AssertionError, match="scatterplot only supports 2D and 3D plots"
     ):
-        dtuplot.scatter(1, 2, 3, 4, show=False)
+        dtuplot.scatter(1, 2, 3, 4, backend=MB, show=False)
     with pytest.raises(
         AssertionError, match="scatterplot only supports 2D and 3D plots"
     ):
-        dtuplot.scatter(1, show=False)
+        dtuplot.scatter(1, backend=MB, show=False)
+    # plotly
+    dtuplot.scatter(1, 2, backend=PB, show=False)
+    dtuplot.scatter(1, 2, 3, backend=PB, show=False)
+    dtuplot.scatter([1], [2], backend=PB, show=False)
+    dtuplot.scatter([[1]], [[2]], backend=PB, show=False)
+    dtuplot.scatter([1, 2, 3], [4, 5, 6], backend=PB, show=False)
+    dtuplot.scatter([1, 2, 3], [4, 5, 6], [7, 8, 9], backend=PB, show=False)
+    dtuplot.scatter(
+        np.array([1, 2, 3]), np.array([4, 5, 6]), np.array([7, 8, 9]), backend=PB, show=False
+    )
+    dtuplot.scatter(np.array([1]), np.array([4]), np.array([7]), backend=PB, show=False)
+    dtuplot.scatter(np.array([1, 2, 3]), np.array([4, 5, 6]), backend=PB, show=False)
+    with pytest.warns(match="np.array with more than 1 dimension"):
+        dtuplot.scatter(np.array([[1, 2, 3]]), np.array([[4, 5, 6]]), backend=PB, show=False)
+    with pytest.warns(match="np.array with more than 1 dimension"):
+        dtuplot.scatter(
+            np.array([[1, 2, 3], [7, 8, 9]]),
+            np.array([[4, 5, 6], [4, 5, 6]]),
+            backend=PB, show=False,
+        )
+    dtuplot.scatter(np.array([1]), np.array([4]), backend=PB, show=False)
+    dtuplot.scatter(
+        Matrix([1, 2, 3]), Matrix([4, 5, 6]), backend=PB, show=False
+    )  # different behavior to the np.array version
+    dtuplot.scatter(Matrix([1, 2]), Matrix([4, 5]), backend=PB, show=False)
+    with pytest.raises(
+        AssertionError, match="scatterplot only supports 2D and 3D plots"
+    ):
+        dtuplot.scatter(1, 2, 3, 4, backend=PB, show=False)
+    with pytest.raises(
+        AssertionError, match="scatterplot only supports 2D and 3D plots"
+    ):
+        dtuplot.scatter(1, backend=PB, show=False)
+    # bokeh
+    dtuplot.scatter(1, 2, backend=BB, show=False)
+    dtuplot.scatter([1], [2], backend=BB, show=False)
+    dtuplot.scatter([[1]], [[2]], backend=BB, show=False)
+    dtuplot.scatter([1, 2, 3], [4, 5, 6], backend=BB, show=False)
+    dtuplot.scatter(np.array([1, 2, 3]), np.array([4, 5, 6]), backend=BB, show=False)
+    dtuplot.scatter(np.array([1]), np.array([4]), backend=BB, show=False)
+    # dtuplot.scatter(
+    #     Matrix([1, 2, 3]), Matrix([4, 5, 6]), backend=BB, show=False
+    # ) 
+    dtuplot.scatter(Matrix([1, 2]), Matrix([4, 5]), backend=BB, show=False)
+    # k3d
+    with pytest.warns(
+        UserWarning, match="K3DBackend only works properly within Jupyter Notebook"
+    ):
+        dtuplot.scatter(1, 2, 3, backend=KB, show=False)
+        dtuplot.scatter([1, 2, 3], [4, 5, 6], [7, 8, 9], backend=KB, show=False)
+        dtuplot.scatter(
+            np.array([1, 2, 3]), np.array([4, 5, 6]), np.array([7, 8, 9]), backend=KB, show=False
+        )
+        dtuplot.scatter(np.array([1]), np.array([4]), np.array([7]), backend=KB, show=False)
+    # mayavi
+    with pytest.warns(
+        DeprecationWarning
+    ):
+        dtuplot.scatter(1, 2, 3, backend=MAB, show=False)
+        dtuplot.scatter([1, 2, 3], [4, 5, 6], [7, 8, 9], backend=MAB, show=False)
+        dtuplot.scatter(
+            np.array([1, 2, 3]), np.array([4, 5, 6]), np.array([7, 8, 9]), backend=MAB, show=False
+        )
+        dtuplot.scatter(np.array([1]), np.array([4]), np.array([7]), backend=MAB, show=False)
 
     # testing arguments
     dtuplot.scatter(
-        1, 2, 3, rendering_kw={"alpha": 1, "s": 100, "color": "black"}, show=False
+        1, 2, 3, rendering_kw={"alpha": 1, "s": 100, "color": "black"}, backend=MB, show=False
     )
-    dtuplot.scatter(1, -1, rendering_kw={"color": "black", "s": 10}, show=False)
+    dtuplot.scatter(1, -1, rendering_kw={"color": "black", "s": 10}, backend=MB, show=False)
     dtuplot.scatter(
         1,
         2,
         rendering_kw={"markersize": 10, "color": "r"},
         xlim=[-2, 2],
         ylim=[-2, 2],
-        show=False,
+        backend=MB, show=False,
     )
 
     # test assertions
     with pytest.raises(
+        NotImplementedError, match="Mayavi does not support 2D scatter plots!"
+    ):
+        dtuplot.scatter([1, 2, 3], [4, 5, 6], backend=MAB, show=False)
+    with pytest.raises(
+        NotImplementedError, match="K3D does not support 2D scatter plots!"
+    ):
+        dtuplot.scatter([1, 2, 3], [4, 5, 6], backend=KB, show=False)
+    with pytest.raises(
+        NotImplementedError, match="Bokeh does not support 3D scatter plots!"
+    ):
+        dtuplot.scatter([1, 2, 3], [4, 5, 6], [7,8,9], backend=BB, show=False)
+    with pytest.raises(
         AssertionError, match="Cannot mix matrix and non-matrix arguments!"
     ):
-        dtuplot.scatter(Matrix([1, 2]), [3, 4], show=False)
+        dtuplot.scatter(Matrix([1, 2]), [3, 4], backend=MB, show=False)
 
     with pytest.raises(
         AssertionError,
         match="Matrix must not have multiple dimensions different from size 1!",
     ):
-        dtuplot.scatter(Matrix([[1, 2, 3], [1, 2, 3]]), Matrix([[1, 2, 3]]), show=False)
+        dtuplot.scatter(Matrix([[1, 2, 3], [1, 2, 3]]), Matrix([[1, 2, 3]]), backend=MB, show=False)
 
     with pytest.raises(AssertionError, match="All matrices must have the same shape!"):
         dtuplot.scatter(
             Matrix([1, 2]),
             Matrix([[1, 2], [2, 3]]),
             Matrix([[1, 2], [2, 3]]),
-            show=False,
+            backend=MB, show=False,
         )
 
     with pytest.warns(match="non-Expr objects in a Matrix is deprecated."):
@@ -263,4 +396,4 @@ def test_scatterplot():
     with pytest.raises(
         AssertionError, match="scatterplot only supports 2D and 3D plots"
     ):
-        dtuplot.scatter(item, item, item, item, show=False)
+        dtuplot.scatter(item, item, item, item, backend=MB, show=False)
